87217d101402f6bd305f85937582b2e2f9dec3fd
Jiayuan-Gu <jigu@eng.ucsd.edu>
2021-04-08 21:34:29 -0700

    [ref] Remove bn option for mlp_bn_relu

--- torkit/nn/modules/mlp.py
+++ torkit/nn/modules/mlp.py
@@ -6,20 +6,19 @@ from .conv import Conv1dBNReLU
 __all__ = ['mlp_bn_relu', 'mlp1d_bn_relu']
 
 
-def mlp_bn_relu(in_channels, out_channels_list, bn=True):
+def mlp_bn_relu(in_channels, out_channels_list):
     c_in = in_channels
     layers = []
     for c_out in out_channels_list:
-        layers.append(LinearBNReLU(c_in, c_out, relu=True, bn=bn))
+        layers.append(LinearBNReLU(c_in, c_out, relu=True, bn=True))
         c_in = c_out
     return nn.Sequential(*layers)
 
 
-def mlp1d_bn_relu(in_channels, out_channels_list, bn=True):
+def mlp1d_bn_relu(in_channels, out_channels_list):
     c_in = in_channels
     layers = []
     for c_out in out_channels_list:
-        layers.append(Conv1dBNReLU(c_in, c_out, 1, relu=True, bn=bn))
+        layers.append(Conv1dBNReLU(c_in, c_out, 1, relu=True))
         c_in = c_out
     return nn.Sequential(*layers)
-
