ab78868a03af15be57495b379ad4dcc24aef4ea4
Jiayuan-Gu <jigu@eng.ucsd.edu>
2021-04-12 10:52:06 -0700

    Add MLP2D

--- torkit/nn/modules/mlp.py
+++ torkit/nn/modules/mlp.py
@@ -1,9 +1,9 @@
 import torch
 from torch import nn
 from .linear import LinearBNReLU
-from .conv import Conv1dBNReLU
+from .conv import Conv1dBNReLU, Conv2dBNReLU
 
-__all__ = ['mlp_bn_relu', 'mlp1d_bn_relu']
+__all__ = ['mlp_bn_relu', 'mlp1d_bn_relu', 'mlp2d_bn_relu']
 
 
 def mlp_bn_relu(in_channels, out_channels_list):
@@ -20,5 +20,14 @@ def mlp1d_bn_relu(in_channels, out_channels_list):
     layers = []
     for c_out in out_channels_list:
         layers.append(Conv1dBNReLU(c_in, c_out, 1, relu=True))
+        c_in = c_out
+    return nn.Sequential(*layers)
+
+
+def mlp2d_bn_relu(in_channels, out_channels_list):
+    c_in = in_channels
+    layers = []
+    for c_out in out_channels_list:
+        layers.append(Conv2dBNReLU(c_in, c_out, 1, relu=True))
         c_in = c_out
     return nn.Sequential(*layers)
